1ea613541bdba1cfe98ed9a4ecfc716c10638851
IanKoech <iankoech9@gmail.com>
2023-05-29 13:02:48 +0300

    :rocket: Created directive for patients with most visits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { DateFormatPipe } from './date-format.pipe';
+import { MostVisitsDirective } from './most-visits.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserFormComponent,
-    DateFormatPipe
+    DateFormatPipe,
+    MostVisitsDirective
   ],
   imports: [
     BrowserModule,
